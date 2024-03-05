5aa097bb227a2b0d490c9a01360d7154189754ea
orrda <104425299+orrda@users.noreply.github.com>
2024-03-05 13:09:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from collection import collection as coll
 import glob
 import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 """
 This script processes a video of a Wilson cloud chamber to detect and clean particles.
@@ -10,76 +13,81 @@ The cleaned particles are then saved to a file.
 """
 
 
+dirctory = "C:\\Users\\orrda\\OneDrive\\Desktop\\שנה ג\\סמסטר א\\מעבדה\\code\\particales\\week1.1\\סרטונים שבוע 1"
+pix_per_mm = 4.5936
+frames_per_second = 30
+duration_range = [130, 280]
+min_length = 200
+
+
 def get_lengths(video_path):
 
     # Create a collection object
     collection = coll(video_path)
 
     # check if the video has already been processed
-    particles_path = video_path[:-4] + "_particles" + ".JSON"
-    #collection.load_particles(particles_path)
+    particles_path = video_path[:-4] + "_particles" + "JSON"
+    collection.load_particles(particles_path)
 
     if collection.arr == []:
-        """ 
-        Detect particles in the video, 
-        the first argument is the minumum ratio between the area intersection of the boxes and their union area,
-        so that it will be considered as the same particle.
-        the second argument is the maximum number of frames between the two boxes to be considered as the same particle.
-        """
-        collection.detect_particles(0.1, 20)
+        # Detect particles in the video
+        collection.detect_particles(
+                min_area_threshold = 20,
+                max_area_threshhold = 2000000,
+                time_threshold=8,
+                first_frame=0, 
+                last_frame=1000000,
+                frame_box=(300, 1900, 0, 1080)
+            )
 
-        # Clean the particles by area, first argument is the minimum area and the second is the maximum area
-        collection.clean_by_area(1000, 20000)
 
-        # Clean the particles by location, if we know that fo example the particles are only in the middle of the frame
-        collection.clean_location(0, 10800, 10, 1000)
-
-        # Clean the particles by brightness
-        collection.clean_by_brightness(2, 255)
-
-        # Clean the particles by duration
-        collection.clean_by_duration(10, 1000)
-
-        # Save the particles to a folder as a json file so next time we can load them from the file, 
-        # and we dont need to detact them again
+        # Save the particles JSON to a file 
         collection.save_particles(particles_path)
-
-        # Save the images of the particles to a folder, can be specified as an argument or in 'collection' class
-        # saves each particle in a image with the name of the video and the frame that the particle was first detected
-        collection.save_particles_images()
-
-
-    durationes = [(p.framerange[1] - p.framerange[0]) for p in collection.arr]
-    lengthes = [p.length() for p in collection.arr]
-    lengthes = [l for l in lengthes if l > 0]
-    return lengthes, durationes
+        
 
 
 
+    # reduce the particles to the ones with the right duration
+    colle = [p for p in collection.arr if duration_range[1] > p.framerange[1] - p.framerange[0] > duration_range[0]]
 
-# put the path to the directory containing the videos here
-dirctory = "C:\\Users\\orrda\\OneDrive\\Desktop\\שנה ג\\סמסטר א\\מעבדה\\code\\particales\\week1.1\\סרטונים שבוע 1"
+    print("Number of particles for 'path'=...", video_path[-9:], ", is - ", len(colle))
+
+    frame = np.zeros((1980, 1080), dtype=np.uint8)
+
+    for p in colle:
+        frame = cv2.drawContours(frame, [np.array(p.contour)], -1, (255, 255, 255), 1)
+
+    frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
+    cv2.imshow("frame", frame)
+    cv2.waitKey(100)
+
+    lengthes = [get_contour_length(np.array(p.contour)) for p in colle]
+    lengthes = [l for l in lengthes if min_length < l]
+
+    return lengthes
+
+
+def get_contour_length(contour):
+    contour = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour, True), True)
+    return cv2.arcLength(contour, True)/2
+
+
+
+
 
 video_paths = glob.glob(dirctory + "\\*.MOV")
 
 all_lengths = []
-all_durationes = []
+
 for path in video_paths:
-    lengths, durations = get_lengths(path)
-    all_lengths = all_lengths + lengths
-    all_durationes = all_durationes + durations
+    all_lengths = all_lengths + get_lengths(path)
 
 
+all_lengths = [l/pix_per_mm for l in all_lengths]
 
-# in the end it will show two histograms, one for the length of the particles and one for the duration of the particles
+
 plt.hist(all_lengths, bins=10)
-plt.xlabel('Particle Length')
+plt.xlabel('Particle Length [mm]')
 plt.ylabel('Frequency')
 plt.title('Length Histogram')
-plt.show()
-
-plt.hist(all_durationes, bins=10)
-plt.xlabel('Particle Duration')
-plt.ylabel('Frequency')
-plt.title('Duration Histogram')
 plt.show()
